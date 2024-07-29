3956d37d538eb3ceefb221c423f61807617eea7b
ArthurMaxWj <arturmwoj@gmail.com>
2024-07-29 14:21:52 +0200

    Add cards to importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,5 @@ pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin_all_from 'app/javascript/shared', under: 'shared'
 pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 # Toastify is linked in 'application.html.erb'
+
+pin_all_from "app/assets/images/cards", under: "cards"
